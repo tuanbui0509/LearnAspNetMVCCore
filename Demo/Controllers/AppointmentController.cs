3e68adb34b58984a2f8365253a8c060328230cd7
tuanbui0509 <tuanbui0509@gmail.com>
2021-06-30 23:20:08 +0700

    routing explained

---
+++ Demo/Controllers/AppointmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //string today = DateTime.Now.ToShortDateString();
+
+            //return Ok(today);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("Okie baby nà aaa " + id);
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -20,7 +20,9 @@ namespace Demo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSetting(
+                    WebHostDefaults.PreventHostingStartupKey, "true")
+                    .UseStartup<Startup>();
                 });
     }
 }
